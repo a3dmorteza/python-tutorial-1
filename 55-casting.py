78bce47fdf82a817732b4582561b2a469b15f2ba
Seyed Morteza Hosseini <a3dmorteza@gmail.com>
2023-03-31 01:30:45 +0330

    add the 55th code

---
+++ 55-casting.py
@@ -0,0 +1,12 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+print("x is:", x)
+print("x type is:", type(x))
+
+print("y is:", y)
+print("y type is:", type(y))
+
+print("z is:", z)
+print("z type is:", type(z))
