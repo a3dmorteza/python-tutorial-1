df37de478a29e238f53b6bf469c258d77657ef08
Seyed Morteza Hosseini <a3dmorteza@gmail.com>
2023-03-31 00:17:19 +0330

    complete the 02 code

--- 02-variables.py
+++ 02-variables.py
@@ -1,6 +1,10 @@
 x = 2
-y = 5
+y = 2
+print("X is: " + str(x))
+print("Y is: " + str(y))
 if x > y:
     print("X is greater than Y!")
+elif x == y:
+    print("X is equal to Y!")
 else:
     print("Y is greater than X!")
