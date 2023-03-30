f975f811881121fa6a2def0c2205f9c17ef4056e
Seyed Morteza Hosseini <a3dmorteza@gmail.com>
2023-03-31 01:32:33 +0330

    add the 56th code

---
+++ 56-casting.py
@@ -0,0 +1,17 @@
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+print("x is:", x)
+print("x type is:", type(x))
+
+print("y is:", y)
+print("y type is:", type(y))
+
+print("z is:", z)
+print("z type is:", type(z))
+
+print("w is:", w)
+print("w type is:", type(w))
