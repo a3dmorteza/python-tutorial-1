ab2228bde4276aeab8e1bb798c61a6a1d2386002
Seyed Morteza Hosseini <a3dmorteza@gmail.com>
2023-03-31 01:33:30 +0330

    add the 57th code

---
+++ 57-casting.py
@@ -0,0 +1,12 @@
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+print("x is:", x)
+print("x type is:", type(x))
+
+print("y is:", y)
+print("y type is:", type(y))
+
+print("z is:", z)
+print("z type is:", type(z))
